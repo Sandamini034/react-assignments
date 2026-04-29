2a3fc554bb2eefc2a18c4a36ab43c87d538bf3b3
Honali Sandamini <hsandaminih@gmail.com>
2026-04-29 12:21:11 +0530

    created the Home.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
 import Assignment_1 from "./assignments/Assignment_1.jsx";
 import Assignment_2 from "./assignments/Assignment_2.jsx";
+import Home from "./assignments/home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Assignment_1 />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/ASG-01" element={<Assignment_1 />}></Route>
         <Route path="/ASG-02" element={<Assignment_2 />}></Route>
       </Routes>
