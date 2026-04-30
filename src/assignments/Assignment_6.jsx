5655a1bf3f33b5f8b8d5ad89e4102ebb102a3d09
Honali Sandamini <hsandaminih@gmail.com>
2026-04-30 12:06:51 +0530

    modified the Assignment_6.jsx

--- src/assignments/Assignment_6.jsx
+++ src/assignments/Assignment_6.jsx
@@ -15,21 +15,39 @@ function Assignment_6(){
         setRuleName("");
         setRuleValue("");
     }
+
+    const del=(index)=>{
+        const newStyle = style.filter((_,i)=>i!==index);
+        setStyle(newStyle);
+    }
+
     return(
         <>
+        <br></br>
         <div className="box4">
+        <h2>Style</h2>
+            <div className="inputBox">
             <input type="text" placeholder="rule name" value={ruleName} 
-            onChange={(e)=>setRuleName(e.target.value)}
-            ></input>
+            onChange={(e)=>setRuleName(e.target.value)}></input>
             <input type="text" placeholder="rule value" value={ruleValue}
-            onChange ={(e)=>setRuleValue(e.target.value)}
-            ></input>
+            onChange ={(e)=>setRuleValue(e.target.value)}></input>
+            </div>
             <button onClick={()=>add()}>Add</button>
+            <br></br>
             {style.map((item,index)=>(
                 <li key={index}>
-                    {item.ruleName} : {item.ruleValue}
+                    {item.ruleName} : "{item.ruleValue}" 
+                    <button onClick={()=>del(index)}>Delete</button>
                 </li>
             ))}
+            <br></br>
+            <div style={style.reduce((obj,item)=>({
+                ...obj,
+                [item.ruleName]:item.ruleValue
+            }),{})}>
+                This text is styled.
+            </div>
+
         </div>
         </>
     )
