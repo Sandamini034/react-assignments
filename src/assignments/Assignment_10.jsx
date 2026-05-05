968d0ce97c5d50871ce75ad215588634abb2d9dc
Honali Sandamini <hsandaminih@gmail.com>
2026-05-05 14:14:19 +0530

    Modified the Assignment_11.jsx
    Add useData , state for storing user data got from the access-token

--- src/assignments/Assignment_10.jsx
+++ src/assignments/Assignment_10.jsx
@@ -1,41 +1,58 @@
 import axios from "axios";
-import {useState} from "react";
+import { useState } from "react";
 import "./Assignment_10.css";
 
-function Assignment_10(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [msg, setMsg] = useState("");
+function Assignment_10() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [disabled, setDisabled] = useState(false);
 
   const login = () => {
-    axios.post("https://auth.dnjs.lk/api/login", {
+    setDisabled(true);
+    axios
+      .post("https://auth.dnjs.lk/api/login", {
         email: email,
-        password: password
-    }).then((response) => {
+        password: password,
+      })
+      .then((response) => {
         console.log(response.data);
         setMsg("Login Successful");
-    }).catch((error) => {
+        setDisabled(false);
+      })
+      .catch((error) => {
         //console.log(error.response.data.error.message);
-        setMsg(error?.response?.data?.error?.message??"An error occurred");
-    });
-  }
+        setMsg(error?.response?.data?.error?.message ?? "An error occurred");
+        setDisabled(false);
+      });
+  };
 
-
-  
-return(
+  return (
     <>
-    <div className="login-box">
+      <div className="login-box">
         <h1>Login</h1>
-        <input type="email" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
-        <input type="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
-        <button onClick={login}>Login</button>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={disabled}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={disabled}
+        ></input>
+        <button onClick={login} disabled={disabled}>
+          Login
+        </button>
 
-        {msg && (
-                <h3>{msg}</h3>  
-        )}
-    </div>
+        {msg && <h3>{msg}</h3>}
+      </div>
     </>
-)
+  );
 }
 
 export default Assignment_10;
--- src/assignments/Assignment_11.jsx
+++ src/assignments/Assignment_11.jsx
@@ -1,56 +1,91 @@
 import axios from "axios";
-import {useState} from "react";
+import { useState } from "react";
+import "./Assignment_11.css";
 
-function Assignment_11(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [msg, setMsg] = useState("");
+function Assignment_11() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [userData, setUserData] = useState(null);
 
   const login = () => {
-    axios.post("https://auth.dnjs.lk/api/login", {
+    setDisabled(true);
+    axios
+      .post("https://auth.dnjs.lk/api/login", {
         email: email,
-        password: password
-    }).then((response) => {
+        password: password,
+      })
+      .then((response) => {
         console.log(response.data);
         setMsg("Login Successful");
+        setLoggedIn(true);
+        setDisabled(false);
 
         axios
-        .get("https://auth.dnjs.lk/api/user", {
-          headers: {
-            Authorization: `Bearer ${response.data.access_token}`,
-          },
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.error(error);
-        })
-        .finally(() => {
-          console.log("Request completed");
-        });
-    }).catch((error) => {
-        setMsg(error.message);
-    });
+          .get("https://auth.dnjs.lk/api/user", {
+            headers: {
+              Authorization: `Bearer ${response.data.access_token}`,
+            },
+          })
+          .then((response) => {
+            console.log(response.data);
+            setUserData(response.data);
+          })
+          .catch((error) => {
+            console.error(error);
+          })
+          .finally(() => {
+            console.log("Request completed");
+          });
+      })
+      .catch((error) => {
+        setMsg(error?.response?.data?.error?.message ?? "An error occurred");
+        setDisabled(false);
+      });
+  };
 
-  }
-
-
-  
-return(
+  return (
     <>
-    <div className="login-box">
-        <h1>Login</h1>
-        <input type="email" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
-        <input type="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
-        <button onClick={login}>Login</button>
+      {loggedIn ? (
+        <div className="container2">
+          {userData ? (
+            <>
+              <h1>{userData.name}</h1>
+              <img src={userData.avatar}></img>
+              <pre>{JSON.stringify(userData, null, 1)}</pre>
+            </>
+          ) : (
+            <p>Loading user data...</p>
+          )}{" "}
+        </div>
+      ) : (
+        <div className="login-box">
+          <h1>Login</h1>
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={disabled}
+          ></input>
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={disabled}
+          ></input>
+          <button onClick={login} disabled={disabled}>
+            Login
+          </button>
 
-        {msg && (
-                <h3>{msg}</h3>  
-        )}
-    </div>
+          {msg && <h3>{msg}</h3>}
+        </div>
+      )}
     </>
-)
+  );
 }
 
 export default Assignment_11;
