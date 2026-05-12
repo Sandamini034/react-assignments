53a51b6c13a9cdfdda65e69da58ff92fa1ef0bbd
Honali Sandamini <hsandaminih@gmail.com>
2026-05-12 13:47:42 +0530

    Modified the Assginment_17.jsx
    Add separate functions hexToRGB and mixing for avoiding repeating code parts

--- src/assignments/Assignment_17.jsx
+++ src/assignments/Assignment_17.jsx
@@ -1,47 +1,33 @@
 import "./Assignment_17.css";
-import { useState} from "react";
+import { useState } from "react";
+
+const hexToRgb = (hex) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return { r, g, b };
+};
+
+const mixing = (percentage, color1, color2) => {
+  const r = Math.round((1 - percentage) * color1.r + percentage * color2.r)
+    .toString(16)
+    .padStart(2, "0");
+  const g = Math.round((1 - percentage) * color1.g + percentage * color2.g)
+    .toString(16)
+    .padStart(2, "0");
+  const b = Math.round((1 - percentage) * color1.b + percentage * color2.b)
+    .toString(16)
+    .padStart(2, "0");
+  return `#${r}${g}${b}`;
+};
 
 const colorMixer = (color1, color2, range) => {
-  const r1 = parseInt(color1.slice(1, 3), 16);
-  const g1 = parseInt(color1.slice(3, 5), 16);
-  const b1 = parseInt(color1.slice(5, 7), 16);
-
-  const r2 = parseInt(color2.slice(1, 3), 16);
-  const g2 = parseInt(color2.slice(3, 5), 16);
-  const b2 = parseInt(color2.slice(5, 7), 16);
+  const { r: r1, g: g1, b: b1 } = hexToRgb(color1);
+  const { r: r2, g: g2, b: b2 } = hexToRgb(color2);
 
   const ratio = range / 100;
   console.log(ratio);
-  let rMixed;
-  let gMixed;
-  let bMixed;
-
-  if (r1 + (r2 - r1) <= 255) {
-    rMixed = Math.round(r1 * ratio + r2 * (1 - ratio))
-      .toString(16)
-      .padStart(2, "0");
-  } else {
-    rMixed = "ff";
-  }
-
-  if (g1 + (g2 - g1) <= 255) {
-    gMixed = Math.round(g1 * ratio + g2 * (1 - ratio))
-      .toString(16)
-      .padStart(2, "0");
-  } else {
-    gMixed = "ff";
-  }
-
-  if (b1 + (b2 - b1) <= 255) {
-    bMixed = Math.round(b1 * ratio + b2 * (1 - ratio))
-      .toString(16)
-      .padStart(2, "0");
-  } else {
-    bMixed = "ff";
-  }
-
-  console.log(rMixed, gMixed, bMixed);
-  return `#${rMixed}${gMixed}${bMixed}`;
+  return mixing(ratio, { r: r1, g: g1, b: b1 }, { r: r2, g: g2, b: b2 });
 };
 
 function Assignment_17() {
