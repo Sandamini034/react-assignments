313d172e1457445a6745587d98cd7cc9e6ed2977
Honali Sandamini <hsandaminih@gmail.com>
2026-05-12 13:38:47 +0530

    Modified the Assignment_18.jsx

--- src/assignments/Assignment_18.jsx
+++ src/assignments/Assignment_18.jsx
@@ -1,39 +1,99 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect, useRef } from "react";
+import "./Assignment_18.css";
 
+function Assignment_18() {
+  const [colors, setColors] = useState(["blue"]);
+  const [gameOver, setGameOver] = useState(null);
+  const intervalRef = useRef(null);
 
-function Assignment_18(){
-    const [colors, setColors]= useState(["blue"]);;
+  function stopGame(message) {
+    clearInterval(intervalRef.current);
+    setGameOver(message);
+  }
 
-    function addColor(){
-        const number = Math.random();
-        console.log(number);
-        const color = number < 0.5 ? "blue" : "red";
-        setColors(prev=>{
-            const newColors = [...prev];
-            newColors.unshift(color);
-            return newColors;
-        });
+  function addColor() {
+    const number = Math.random();
+    console.log(number);
+    const color = number < 0.5 ? "blue" : "red";
+    setColors((prev) => {
+      const newColors = [...prev];
+      newColors.unshift(color);
+      return newColors;
+    });
+  }
 
+  function removeColor(color) {
+    const newColors = [...colors];
+    if (newColors[newColors.length - 1] === color) {
+      newColors.pop();
+      setColors(newColors);
+    } else if (newColors[newColors.length - 1] !== color) {
+      stopGame("You lost the game!");
     }
-    useEffect(()=>{
-        const interval = setInterval(addColor, 1000);
-        return () => clearInterval(interval);
-    },[]);
 
-    return(
-        <div className="colorMixer1">
-            <div style={{ display: 'flex' , flexWrap:`wrap`}}>  {}
-        {colors.map((color, index) => (
-            <div key={index} style={{
-                backgroundColor: color,  
-                width: '50px',
-                height: '50px',
-            }}>
-            </div>
-        ))}
-    </div>
+    if (newColors.length === 0) {
+      stopGame("Congratulations! You won the game!");
+    }
+  }
+
+  useEffect(() => {
+    intervalRef.current = setInterval(addColor, 1000);
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  useEffect(() => {
+    if (colors.length > 7) {
+      stopGame("You lost the game!");
+    }
+  }, [colors]);
+
+  return (
+    <div className="colorMixer1">
+      {gameOver ? (
+        <div>
+          <h1>{gameOver}</h1>
         </div>
-    )
+      ) : (
+        <>
+          <div style={{ display: "flex", flexWrap: `wrap` }}>
+            {" "}
+            {}
+            {colors.map((color, index) => (
+              <div
+                id="color1"
+                key={index}
+                style={{
+                  backgroundColor: color,
+                  width: "50px",
+                  height: "50px",
+                }}
+              ></div>
+            ))}
+          </div>
+          <div className="buttonBox">
+            <button
+              id="redButton"
+              value="red"
+              onClick={(e) => {
+                removeColor(e.target.value);
+              }}
+            >
+              red
+            </button>
+            <button
+              id="blueButton"
+              value="blue"
+              onClick={(e) => {
+                removeColor(e.target.value);
+              }}
+            >
+              blue
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Assignment_18;   
+export default Assignment_18;
